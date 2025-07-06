14aa3a2ede0d9331634b591b74fa4c1a4ec4c8c5
rou-nak-cloud <github.rounak.19@gmail.com>
2025-07-06 14:43:18 +0530

    AppContext

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Login = () => {
 
+  const navigate = useNavigate();
+
+  const { backendUrl, setIsLoggedIn } = useContext(AppContext)
+
   const [state, setState] = useState('Sign Up')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-1 bg-gradient-to-br from-blue-200/90 to bg-purple-400/60'>
-      <img src={assets.logo} alt="Logo" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+      <img 
+      onClick={() => navigate('/')}
+      src={assets.logo} alt="Logo" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
       <div className='bg-slate-900/95 p-10 rounded-lg shadow-md w-full sm:w-96 text-indigo-300 text-sm'>
         <h2 className='text-3xl font-semibold text-white text-center mb-1'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
         <p className='text-sm font-light text-center mb-6'>{state === 'Sign Up' ? 'Create your Account' : 'Login to your account!'}</p>
@@ -16,23 +28,34 @@ const Login = () => {
             {state === 'Sign Up' && ( 
               <div className='mb-4 flex items-center gap-3 w-full px-4 py-2 rounded-3xl bg-[#333A6C]'>
               <img src={assets.person_icon} alt="input logo" />
-              <input type="text" placeholder='Full Name' required 
+              <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text" placeholder='Full Name' required 
               className='bg-transparent outline-none text-white' />
             </div>
             )}
            
             <div className='mb-4 flex items-center gap-3 w-full px-4 py-2 rounded-3xl bg-[#333A6C]'>
               <img src={assets.mail_icon} alt="input logo" />
-              <input type="email" placeholder='Your Email' required 
+              <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email" placeholder='Your Email' required 
               className='bg-transparent outline-none text-white' />
             </div>
             <div className='mb-4 flex items-center gap-3 w-full px-4 py-2 rounded-3xl bg-[#333A6C]'>
               <img src={assets.lock_icon} alt="input logo" />
-              <input type="password" placeholder='Password' required 
+              <input 
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password" placeholder='Password' required 
               className='bg-transparent outline-none text-white' />
             </div>
 
-            <p className='text-xs pl-2 underline underline-offset-2 mb-4'>Forget your password?</p>
+            <p 
+            onClick={() => navigate('/reset-password')}
+            className='text-xs pl-2 underline underline-offset-2 mb-4'>Forget your password?</p>
             <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500/90 to-indigo-800/90 text-gray-200 font-medium cursor-pointer hover:text-gray-400 transition-all duration-300 ease-in'>
               {state}
             </button>
