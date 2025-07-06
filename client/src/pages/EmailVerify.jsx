4b2c0c3d935dd9575e9f33549e466729728fe040
rou-nak-cloud <github.rounak.19@gmail.com>
2025-07-06 21:29:03 +0530

    Account veriification functionality with OTP

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { AppContext } from '../context/AppContext'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const EmailVerify = () => {
-  const inputRefs = React.useRef([])
+  axios.defaults.withCredentials = true;
+  const {backendUrl, isLoggedIn, userData, getUserData} = useContext(AppContext);
+
+  const navigate = useNavigate();
+  const inputRefs = React.useRef([]);
 
   const handleInput = (e,index) => {
     if(e.target.value.length > 0 && index < inputRefs.current.length -1){
@@ -15,14 +23,36 @@ const EmailVerify = () => {
     }
   }
   const handlePaste = (e) => {
-    const paste = e.clipboardData.getData('text')
+    const paste = e.clipboardData.getData('text').trim()
     const pasteArray = paste.split('');
-    pasteArray.array.forEach((char,index) => {
+    pasteArray.forEach((char,index) => {
       if(inputRefs.current[index]){
         inputRefs.current[index].value = char;
       }
     });
   }
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      const otpArray = inputRefs.current.map(e => e.value)
+      const otp = otpArray.join('')
+
+      const {data} = await axios.post(backendUrl + '/api/auth/verify-email', {otp})
+      if(data.success){
+        toast.success(data.message)
+        getUserData()
+        navigate('/')
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    isLoggedIn && userData && userData.isAccountVerified && navigate('/')
+  },[isLoggedIn, userData])
 
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200/90 to bg-purple-400/60'>
@@ -31,6 +61,7 @@ const EmailVerify = () => {
             src={assets.logo} alt="Logo" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
 
             <form action="#"
+            onSubmit={onSubmitHandler}
             className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'
             >
               <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verification OTP</h1>
